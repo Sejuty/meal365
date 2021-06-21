434305de5d20a033696f58e11ab9b4a92a5ccefd
hel-ium <53427689+hel-ium@users.noreply.github.com>
2021-06-22 05:39:48 +0600

    I have created UI for splashScreen, Login and SignUp page. Changed font. Added a button to return to log in page from homepage.

--- app/src/main/java/com/nishat00/mealmanagement/homepage.java
+++ app/src/main/java/com/nishat00/mealmanagement/homepage.java
@@ -2,13 +2,29 @@ package com.nishat00.mealmanagement;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
 
 public class homepage extends AppCompatActivity {
+
+    ImageView backToLogInButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_homepage);
+
+        backToLogInButton = findViewById(R.id.backToLogIn);
+
+        backToLogInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent homePageToLogInPage = new Intent(homepage.this,log_in.class);
+                startActivity(homePageToLogInPage);
+            }
+        });
     }
 }
