736116d8b8ecda8897ef0cc86886277440618895
Abidstic101 <abid71@student.sust.edu>
2021-06-22 05:39:57 +0600

    connected the app to firebase creating new users authenticating new users

--- app/src/main/java/com/nishat00/mealmanagement/log_in.java
+++ app/src/main/java/com/nishat00/mealmanagement/log_in.java
@@ -1,15 +1,29 @@
 package com.nishat00.mealmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
 
 public class log_in extends AppCompatActivity {
 
     Button loginButton,signUpButton;
+    EditText mEmail,mPassword;
+    FirebaseAuth fAuth;
+    ProgressBar progressBar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,12 +32,60 @@ public class log_in extends AppCompatActivity {
 
         loginButton = findViewById(R.id.login);
         signUpButton = findViewById(R.id.signup);
+        mEmail=findViewById(R.id.emailLogin);
+        mPassword=findViewById(R.id.password);
+        progressBar =findViewById(R.id.progressBar2);
+        fAuth=FirebaseAuth.getInstance();
+        Intent homePage = new Intent(log_in.this,homepage.class);
+
 
         loginButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent homePage = new Intent(log_in.this,homepage.class);
-                startActivity(homePage);
+
+
+
+                String email = mEmail.getText().toString().trim();
+                String password =mPassword.getText().toString().trim();
+
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email is required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    mPassword.setError("Password is required!");
+                    return ;
+
+                }
+
+                if(password.length()<6)
+                {
+                    mPassword.setError("The password should be more than 6 digits");
+                    return;
+                }
+
+                progressBar.setVisibility(View.VISIBLE);
+                //authenticate the user
+                fAuth.signInWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful())
+                        {
+                            Toast.makeText(log_in.this, "Logged in Successfully", Toast.LENGTH_SHORT).show();
+                            startActivity(homePage);
+                            finish();
+
+                        }
+                        else
+                        {
+                            Toast.makeText(log_in.this, "Error"+task.getException().getMessage() , Toast.LENGTH_SHORT).show();
+                        }
+
+                    }
+                });
+
             }
         });
 
--- app/src/main/java/com/nishat00/mealmanagement/signup.java
+++ app/src/main/java/com/nishat00/mealmanagement/signup.java
@@ -1,15 +1,29 @@
 package com.nishat00.mealmanagement;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
 
 public class signup extends AppCompatActivity {
 
     Button signUpButton,loginButtonSignUpPage;
+    EditText userName,mEmail,mPassword,mConfirmPassword;
+    ProgressBar progressBar;
+    FirebaseAuth fAuth;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,15 +32,70 @@ public class signup extends AppCompatActivity {
 
         signUpButton = findViewById(R.id.signupButtonInSignUpPage);
         loginButtonSignUpPage = findViewById(R.id.loginButtonInSignUpPage);
+        userName = findViewById(R.id.userNameSignUp);
+        mEmail =findViewById(R.id.email);
+        mPassword =findViewById(R.id.passwordSignUp);
+        mConfirmPassword=findViewById(R.id.confirmPassword);
+        fAuth = FirebaseAuth.getInstance();
+        progressBar=findViewById(R.id.progressBar);
+        Intent goToSignUpPageToHomePage = new Intent(signup.this,homepage.class);
+        if(fAuth.getCurrentUser()!=null)
+        {
+            startActivity(goToSignUpPageToHomePage);
+            finish();
+
+        }
 
         signUpButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Intent goToSignUpPageToHomePage = new Intent(signup.this,homepage.class);
-                startActivity(goToSignUpPageToHomePage);
 
-            }
+
+                String email = mEmail.getText().toString().trim();
+                String password =mPassword.getText().toString().trim();
+                String confirmPassword =mConfirmPassword.getText().toString().trim();
+                if(TextUtils.isEmpty(email))
+                {
+                    mEmail.setError("Email is required!");
+                    return;
+                }
+                if(TextUtils.isEmpty(password))
+                {
+                    mPassword.setError("Password is required!");
+                    return ;
+
+                }
+
+                if(password.length()<6)
+                {
+                    mPassword.setError("The password should be more than 6 digits");
+                    return;
+                }
+                if(!confirmPassword.equals(password))
+                {
+                    mConfirmPassword.setError("Password doesn't match");
+                    return ;
+                }
+                progressBar.setVisibility(View.VISIBLE);
+                //when we click the signup button
+                fAuth.createUserWithEmailAndPassword(email,password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful() )
+                        {
+                            Toast.makeText(signup.this, "User created", Toast.LENGTH_SHORT).show();
+                            startActivity(goToSignUpPageToHomePage);
+                        }
+                        else
+                        {
+                            Toast.makeText(signup.this, "Error"+task.getException().getMessage() , Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                });
+
+
+            };
         });
 
         loginButtonSignUpPage.setOnClickListener(new View.OnClickListener() {
