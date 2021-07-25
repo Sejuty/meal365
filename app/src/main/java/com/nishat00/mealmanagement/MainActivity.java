539832637a39721439f60f930bdb9e09ae9962b8
Abidstic101 <abid71@student.sust.edu>
2021-07-25 17:58:28 +0600

    added splash screen timeout in place of on click listener

--- app/src/main/java/com/nishat00/mealmanagement/MainActivity.java
+++ app/src/main/java/com/nishat00/mealmanagement/MainActivity.java
@@ -4,11 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private static int SPLASH_TIME_OUT=3000;
 
     TextView start;
 
@@ -17,14 +19,14 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        start = findViewById(R.id.start);
+      new Handler().postDelayed(new Runnable() {
+          @Override
+          public void run() {
+              Intent intent =new Intent(MainActivity.this,log_in.class);
+              startActivity(intent);
+              finish();
 
-        start.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                Intent login = new Intent(MainActivity.this,log_in.class);
-                startActivity(login);
-            }
-        });
+          }
+      },SPLASH_TIME_OUT);
     }
 }
