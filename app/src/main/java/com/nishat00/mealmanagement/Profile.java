27a79271817619d8dca3e17b5c8d067623780fde
hel-ium <53427689+hel-ium@users.noreply.github.com>
2021-07-26 23:50:25 +0600

    icon added

--- app/src/main/java/com/nishat00/mealmanagement/Profile.java
+++ app/src/main/java/com/nishat00/mealmanagement/Profile.java
@@ -7,6 +7,7 @@ import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 import com.google.firebase.auth.FirebaseAuth;
 
